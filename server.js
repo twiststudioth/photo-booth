c96d56db9c6bead9808f2acbd89d575433287cb0
Thanapong Chodchoi (Yumat) Fullstack Developer <thanapong.dev@myexpress.ai>
2026-05-11 21:20:52 +0700

    Disable rate limiter

--- server.js
+++ server.js
@@ -46,11 +46,11 @@ app.use(helmet({
 app.use(compression());
 
 // Rate limiting
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100 // limit each IP to 100 requests per windowMs
-});
-app.use('/api/', limiter);
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 100 // limit each IP to 100 requests per windowMs
+// });
+// app.use('/api/', limiter);
 
 // Body parser
 app.use(express.json({ limit: '50mb' }));
